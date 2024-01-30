800067ca1319924064270f3439017325f64d0e4a
Dhruvil Sheth <dhruvilsheth@proton.me>
2024-01-30 13:35:44 +0530

    done

---
+++ quiz/src/components/HomeComponent.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "../styles/Home.css";
+import { NavLink } from "react-router-dom";
+
+function HomeComponent() {
+  return (
+    <div className="flex home-container">
+      <h1>Quiz App</h1>
+      <NavLink to="/Quiz">
+        <button className="play-button">Play</button>
+      </NavLink>
+    </div>
+  );
+}
+
+export default HomeComponent;
